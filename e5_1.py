0aaa84a3269d962d3af8dcd062481e6cf53f92eb
adamczarnecki <adam@czarnecki.cz>
2019-12-05 09:46:53 +0100

    5_1 new opcodes

--- e5_1.py
+++ e5_1.py
@@ -4,20 +4,28 @@ def intcode_computer(instructions):
     halt = 99
     add = 1
     multiply = 2
-    for pos in range(0, len(intcode), 4):
-        # adresy:
+    inp = 3
+    out = 4
+    # The Loop:
+    pos = 0
+    while True:
         job = intcode[pos]
-        a = intcode[intcode[pos + 1]]
-        b = intcode[intcode[pos + 2]]
-        res_pos = intcode[pos + 3]
         # logika:
         if job == halt:
             return intcode[0]
         elif job == add:
-            intcode[res_pos] = a + b
+            intcode[pos + 3] = intcode[pos + 1] + intcode[pos + 2]
+            pos += 4
         elif job == multiply:
-            intcode[res_pos] = a * b
-        # print('Pozycja: ', pos, 'zadanie: ', job, 'A:', a, 'B:', b, 'Adres zapisu: ', res_pos)
+            intcode[pos + 3] = intcode[pos + 1] * intcode[pos + 2]
+            pos += 4
+        elif job == inp:
+            intcode[pos + 1] = int(input('input: '))
+            pos += 2
+        elif job == out:
+            print(intcode[pos + 1])
+            pos +=2
+    # print('Pozycja: ', pos, 'zadanie: ', job, 'A:', a, 'B:', b, 'Adres zapisu: ', res_pos)
 
     return intcode[0]
 
@@ -26,8 +34,10 @@ if __name__ == '__main__':
     import time
     start = time.time()
     with open('inputs/5.in') as f:
-        intcode = f.read().split(',')
-    for i, x in enumerate(intcode):
-        intcode[i] = int(x)
+        intcode = [int(x) for x in f.read().split(',')]
+
+    # for debuging
+    # intcode = [3, 0, 4, 0, 99]
+    print(intcode_computer(intcode))
 
     print('czas: ', time.time() - start)
