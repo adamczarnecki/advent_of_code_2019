5706aae549703d718689973fb22450453e201fb3
adamczarnecki <adam@czarnecki.cz>
2019-12-05 14:50:23 +0100

    fuuuuuuuck but works

--- e5_1.py
+++ e5_1.py
@@ -9,24 +9,59 @@ def intcode_computer(instructions):
     # The Loop:
     pos = 0
     while True:
-        job = intcode[pos]
-        # logika:
+        # opcode and job
+        # opcode always 5 digits
+        opcode = str(intcode[pos])
+        if len(opcode) < 5:
+            opcode = '0' * (5 - len(opcode)) + opcode
+        print('pos: ', pos, 'opcode: ', opcode)
+        opcode = [int(x) for x in opcode]
+        if opcode[3]:
+            job = 99
+        else:
+            job = opcode[4]
+        mode1 = opcode[2]
+        mode2 = opcode[1]
+    # logic
         if job == halt:
-            # print('Pozycja: ', pos, 'zadanie: ', job)
             return intcode[0]
         elif job == add:
-            # print('Pozycja: ', pos, 'zadanie: ', job, 'adres: ', intcode[pos + 1])
-            intcode[intcode[pos + 3]] = intcode[intcode[pos + 1]] + intcode[intcode[pos + 2]]
+            if mode1:
+                a = intcode[pos + 1]
+            else:
+                a = intcode[intcode[pos + 1]]
+            if mode2:
+                b = intcode[pos + 2]
+            else:
+                b = intcode[intcode[pos + 2]]
+            intcode[intcode[pos + 3]] = a + b
             pos += 4
         elif job == multiply:
-            intcode[intcode[pos + 3]] = intcode[intcode[pos + 1]] * intcode[intcode[pos + 2]]
+            if mode1:
+                a = intcode[pos + 1]
+            else:
+                a = intcode[intcode[pos + 1]]
+            if mode2:
+                b = intcode[pos + 2]
+            else:
+                b = intcode[intcode[pos + 2]]
+            intcode[intcode[pos + 3]] = a * b
             pos += 4
         elif job == inp:
             intcode[intcode[pos + 1]] = int(input('input: '))
             pos += 2
         elif job == out:
-            print(intcode[intcode[pos + 1]])
+            if mode1:
+                a = intcode[pos + 1]
+            else:
+                a = intcode[intcode[pos + 1]]
+            print(a)
             pos += 2
+        else:
+            print('IntcodeError:', job, 'is not a valid opcode.')
+            return intcode[0]
+        if pos > len(intcode) - 1:
+            break
 
     return intcode[0]
 
@@ -39,7 +74,9 @@ if __name__ == '__main__':
 
     # for debuging
     #         inp    inp   add         out   multiply    halt
-    intcode = [3, 1, 3, 3, 1, 1, 3, 0, 4, 0, 2, 1, 3, 0, 99]   # return multiplication of inputs
+    # intcode = [3, 1, 3, 3, 1, 1, 3, 0, 4, 0, 2, 1, 3, 0, 99]   # return multiplication of inputs
+    # intcode = [1002, 4, 3, 0, 33]       # 99, not end properly
+    #intcode = [1101, 100, -1, 0]        # 99, end properly
     print(intcode_computer(intcode))
 
     print('czas: ', time.time() - start)
